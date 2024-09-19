c1cafea2e081189adfbefe250b55b3da844b0ab4
Shakhron Komilov <leader.pro11@gmail.com>
2024-09-19 17:58:37 +0500

    fix

--- HW04/hw04Result/add-product.js
+++ HW04/hw04Result/add-product.js
@@ -1,22 +1,23 @@
-// const addProduct = (product, products) => products.push(product);
-// const products = [
-//   { name: "Laptop", price: 999.99, quantity: 5 },
-//   { name: "Phone", price: 499.99, quantity: 10 },
-// ];
-// const newProduct = { name: "Tablet", price: 299.99, quantity: 7 };
-// console.log(addProduct(newProduct, products));
-
-function addProduct(product, products) {
-  products.push(product);
-  return products;
-}
-
+const addProduct = (product, products) => (products.push(product), products);
 const products = [
   { name: "Laptop", price: 999.99, quantity: 5 },
   { name: "Phone", price: 499.99, quantity: 10 },
 ];
-
 const newProduct = { name: "Tablet", price: 299.99, quantity: 7 };
-
 console.log(addProduct(newProduct, products));
 
+// ----- дебликат
+
+// function addProduct(product, products) {
+//   products.push(product);
+//   return products;
+// }
+
+// const products = [
+//   { name: "Laptop", price: 999.99, quantity: 5 },
+//   { name: "Phone", price: 499.99, quantity: 10 },
+// ];
+
+// const newProduct = { name: "Tablet", price: 299.99, quantity: 7 };
+
+// console.log(addProduct(newProduct, products));

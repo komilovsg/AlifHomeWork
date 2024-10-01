f9b2a292b220befc790de0c7dc22fa1f320c095f
Shakhron Komilov <komilovsg@gmail.com>
2024-10-01 08:46:42 -0700

    fix it

---
+++ ClassWork/index.js
@@ -0,0 +1,20 @@
+// const str = "My name is 1234567890;"
+// const regex = /\d/g;
+// console.log(str.match(regex));
+
+// const regex = /h.llo/g;
+// const str = "hello htllo hrlll hhr"
+// console.log(regex.match(str));
+
+// изучить дома сегодняшнюю тему , а то в классе не очень следил за темой, поэтому остались вопросы. 
+// для этого нужно - полностью взять повторить все пройденные темы,
+//  а после чего по каждой из них пройти по примерам, без использования чата GTP
+
+// NEED to install NVM to this MacBook today
+
+const regex = /^Hello/m;
+const str = `Hello, world?
+Hello again!
+Goodbuy`;
+
+console.log(str.match(regex));

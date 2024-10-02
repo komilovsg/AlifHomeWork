84ae847996c44b611048d434997d74da291e0397
Shakhron Komilov <komilovsg@gmail.com>
2024-10-02 03:37:40 -0700

    2 tasks done

---
+++ HW08/containsDigit.js
@@ -0,0 +1,17 @@
+function containsDigit(str) {
+    const regex = /\d/;
+    return regex.test(str);
+}
+
+const strings = [
+    'Hello World!', 
+    'Today is 2024.',           
+    'My lucky number is 7.',    
+    'No digits here!',           
+    'Price: $50.',              
+    '',                          
+];
+
+strings.forEach(s => {
+    console.log(`Строка: "${s}", Содержит цифры: ${containsDigit(s)}`);
+});
---
+++ HW08/containsWord.js
@@ -0,0 +1,18 @@
+function containsWord(str) {
+    const regex = /\b(cat|dog|brid)\b/;
+    return regex.test(str);
+}
+
+
+const strings = [
+    'I have a cat.',            
+    'The dog is barking.',      
+    'A bird is flying.',        
+    'There is a fish.',         
+    'The catalog is huge.',     
+    '',                         
+];
+
+strings.forEach(s => {
+    console.log(`Строка: "${s}", Содержит одно из слов: ${containsWord(s)}`);
+});
